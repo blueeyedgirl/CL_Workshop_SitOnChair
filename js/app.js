eca3616dde2b35d7a90c9f51c6fd492be0f6f0b1
blueeyedgirl <opalka.marta@gmail.com>
2017-04-07 14:45:26 +0200

    slider skończony

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function(){
     var oFirmie = document.querySelector(".navigation ul li");
     console.log(oFirmie);
 
+    // submenu
+
     oFirmie.addEventListener("mouseover", function(event){
         var submenu = this.querySelector("ul");
         var triangle = this.querySelector("#triangle");
@@ -23,15 +25,15 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     });
 
+    // bloki z obrazkami
+
     var imgParent = document.querySelector("#section2 .siteWidth .absolutePositioning .centerElements");
-    console.log(imgParent);
+    // console.log(imgParent);
 
     var imgParentChildren = imgParent.children;
-    console.log(imgParentChildren);
-
+    // console.log(imgParentChildren);
 
     for (var i=0; i<imgParentChildren.length-1; i++) {
-        console.log(imgParentChildren[i]);
         imgParentChildren[i].addEventListener("mouseover", function(event){
             var transparentStrip = this.querySelector(".transparentStrip");
             transparentStrip.style.display = "none";
@@ -42,5 +44,35 @@ document.addEventListener("DOMContentLoaded", function(){
             transparentStrip.style.display = "block";
         });
     }
+
+    // slider
+
+    var slider = document.querySelector(".slider");
+    var buttonBack = document.querySelector(".back");
+    var buttonForth = document.querySelector(".forth");
+    var sliderLis = slider.children;
+
+
+
+    var pictureCounter = 0;
+    var visibleLi = sliderLis[pictureCounter].classList.add("visible");
+
+    buttonForth.addEventListener("click", function(event){
+        sliderLis[pictureCounter].classList.remove("visible");
+        pictureCounter++;
+        if (pictureCounter >= sliderLis.length){
+            pictureCounter = 0;
+        }
+        sliderLis[pictureCounter].classList.add("visible");
+     });
+
+     buttonBack.addEventListener("click", function(event){
+         sliderLis[pictureCounter].classList.remove("visible");
+         pictureCounter--;
+         if (pictureCounter < 0){
+             pictureCounter = 2;
+         }
+         sliderLis[pictureCounter].classList.add("visible");
+      });
 
 });
